4868c4a0f409c1e4fdbbff96c1268a59926016fe
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-24 20:13:16 -0300

    SongDetails page component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -18,35 +18,37 @@ const TopChartCard = ({
   activeSong,
   handlePauseClick,
   handlePlayClick,
-}) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+}) => {
+  return (
+    <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+      <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
 
-    <div className="flex-1 flex flex-row justify-between items-center">
-      <img
-        src={song?.images?.coverart}
-        alt={song?.title}
-        className="w-20 h-20 rounded-lg"
-      />
-      <div className="flex-1 flex flex-col justify-center mx-3">
-        <Link to={`/songs/${song.key}`}>
-          <p className="text-xl font-bold text-white">{song?.title}</p>
-        </Link>
-        <Link to={`/artists/${song?.artists[0].adamid}`}>
-          <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
-        </Link>
+      <div className="flex-1 flex flex-row justify-between items-center">
+        <img
+          src={song?.images?.coverart}
+          alt={song?.title}
+          className="w-20 h-20 rounded-lg"
+        />
+        <div className="flex-1 flex flex-col justify-center mx-3">
+          <Link to={`/songs/${song.key}`}>
+            <p className="text-xl font-bold text-white">{song?.title}</p>
+          </Link>
+          <Link to={`/artists/${song?.artists[0].adamid}`}>
+            <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
+          </Link>
+        </div>
+
+        <PlayPause
+          isPlaying={isPlaying}
+          activeSong={activeSong}
+          song={song}
+          handlePause={handlePauseClick}
+          handlePlay={handlePlayClick}
+        />
       </div>
-
-      <PlayPause
-        isPlaying={isPlaying}
-        activeSong={activeSong}
-        song={song}
-        handlePause={handlePauseClick}
-        handlePlay={handlePlayClick}
-      />
     </div>
-  </div>
-);
+  );
+};
 
 const TopPlay = () => {
   const dispatch = useDispatch();
