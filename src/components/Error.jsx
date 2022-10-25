e95cb978adce35087eaf302e614d00003cc07ea8
GabrielDoddy94 <gabrieldoddy1994@gmail.com>
2022-10-25 18:35:56 -0300

    Search component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Error = () => (
   <div className="w-full flex justify-center items-center">
-    <h1>Something went wrong. Please try again.</h1>
+    <h1 className="font-bold text-2xl text-white mt-2">
+      Something went wrong. Please try again.
+    </h1>
   </div>
 );
 
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,13 +2,18 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { selectGenreListId } from "../redux/features/playerSlice";
+import { useGetSongsByGenreQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
   const dispatch = useDispatch();
-  const { activeSong, isPlaying } = useSelector(state => state.player);
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = "Pop";
+  const { activeSong, isPlaying, genreListId } = useSelector(
+    state => state.player
+  );
+  const { data, isFetching, error } = useGetSongsByGenreQuery(
+    genreListId || "POP"
+  );
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   if (isFetching) return <Loader title="Loading songs..." />;
 
@@ -21,8 +26,8 @@ const Discover = () => {
           Discover {genreTitle}
         </h2>
         <select
-          onChange={() => {}}
-          value=""
+          onChange={e => dispatch(selectGenreListId(e.target.value))}
+          value={genreListId || "POP"}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map(genre => (
